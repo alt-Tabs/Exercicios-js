4ba0510fa23141e946f4d481ebfae24300aa204d
devtabs <devtabs2019@gmail.com>
2019-12-11 09:51:36 -0300

    deleting files

--- node/middlewares.js
+++ node/middlewares.js
@@ -1,4 +1,5 @@
 //middleware pattern (chain of responsibility)
+//recebe, request, response e next (opcional)
 const passo1 = (ctx, next)=>{
     ctx.valor1 = 'mid1'
     next()
@@ -6,6 +7,19 @@ const passo1 = (ctx, next)=>{
 
 const passo2 = (ctx, next) =>{
     ctx.valor2 = 'mid2'
+    next()
 }
 
 const passo3 = ctx => ctx.valor3 = 'mid3'
+
+const exec = (ctx, ...middlewares) => { // ...middlewares (conjunto de funções middleware) "..." operador rest= junta todas as funções dentro de um array, no caso "middlewares"
+    const execPasso = indice => {
+        middlewares && indice < middlewares.length &&
+            middlewares [indice] (ctx,()=> execPasso(indice+1 ))
+    }
+    
+    execPasso(0)
+}
+const ctx = {}
+exec(ctx, passo1, passo2, passo3)
+console.log(ctx)
